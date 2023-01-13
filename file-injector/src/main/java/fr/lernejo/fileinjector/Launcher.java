a520aa7dc280b53cf901babf7c9b5c4b0f0fc045
quentin1.guyot <quentin1.guyot@epita.fr>
2023-01-13 16:17:45 +0100

    Correction Launcher file injector

--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,7 +1,5 @@
 package fr.lernejo.fileinjector;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
@@ -20,33 +18,21 @@ import java.util.List;
 @SpringBootApplication
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-            System.out.println("Hello after starting Spring");
-            if (args.length == 0)
-                throw new RuntimeException("Please add path file");
-
-            List<GameInfo> gameInfos =
-                    Arrays.asList(new ObjectMapper().readValue(Paths.get(args[0]).toFile(), GameInfo[].class));
-            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
-
-            for (GameInfo gameInfo : gameInfos) {
-                rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
-                rabbitTemplate.convertAndSend("", "game_info", gameInfo,
-                        new MessagePostProcessor() {
-                            @Override
-                            public Message postProcessMessage(Message message) throws AmqpException {
-                                message.getMessageProperties().getHeaders().put("game_id", gameInfo.id());
-                                return message;
-                            }
-                        });
+            if (args.length > 0) {
+                List<GameInfo> gameInfos = Arrays.asList(new ObjectMapper().readValue(Paths.get(args[0]).toFile(), GameInfo[].class));
+                RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+                for (GameInfo gameInfo : gameInfos) {
+                    rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+                    rabbitTemplate.convertAndSend("", "game_info", gameInfo, new MessagePostProcessor() {
+                                @Override
+                                public Message postProcessMessage(Message message) throws AmqpException {
+                                    message.getMessageProperties().getHeaders().put("game_id", gameInfo.id());
+                                    return message;
+                                }});
+                }
             }
-        } catch (StreamReadException e) {
-            throw new RuntimeException(e);
-        } catch (DatabindException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
