417886dce4d546b776b14b554f2f8a6ddbc8f4f6
quentin1.guyot <quentin1.guyot@epita.fr>
2023-01-13 16:02:06 +0100

    Partie 4

--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,52 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
             System.out.println("Hello after starting Spring");
+            if (args.length == 0)
+                throw new RuntimeException("Please add path file");
+
+            List<GameInfo> gameInfos =
+                    Arrays.asList(new ObjectMapper().readValue(Paths.get(args[0]).toFile(), GameInfo[].class));
+            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+
+            for (GameInfo gameInfo : gameInfos) {
+                rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+                rabbitTemplate.convertAndSend("", "game_info", gameInfo,
+                        new MessagePostProcessor() {
+                            @Override
+                            public Message postProcessMessage(Message message) throws AmqpException {
+                                message.getMessageProperties().getHeaders().put("game_id", gameInfo.id());
+                                return message;
+                            }
+                        });
+            }
+        } catch (StreamReadException e) {
+            throw new RuntimeException(e);
+        } catch (DatabindException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
