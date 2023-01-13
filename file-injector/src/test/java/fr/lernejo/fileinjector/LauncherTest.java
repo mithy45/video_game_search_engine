8e011f247fb2f9915a0de733c31ad36d01dcf7ed
quentin1.guyot <quentin1.guyot@epita.fr>
2023-01-13 16:57:18 +0100

    Renaming launcher test class

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -2,11 +2,7 @@ package fr.lernejo.fileinjector;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class auncherTest {
+class LauncherTest {
 
     @Test
     void main_terminates_before_5_sec() {
