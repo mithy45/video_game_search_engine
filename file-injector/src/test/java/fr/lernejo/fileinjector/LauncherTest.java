bca9eaeb6cd70032aec0333e507285fde68aae0e
quentin1.guyot <quentin1.guyot@epita.fr>
2023-01-13 17:11:28 +0100

    Adding test in file injector

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -2,9 +2,16 @@ package fr.lernejo.fileinjector;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
 class LauncherTest {
 
     @Test
     void main_terminates_before_5_sec() {
+        assertTimeoutPreemptively(
+                Duration.ofSeconds(0),
+                () -> Launcher.main(new String[]{}));
     }
 }
